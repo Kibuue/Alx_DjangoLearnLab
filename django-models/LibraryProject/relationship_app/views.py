f8f60fd8ac85e5b25b024a8a6c7369a525a0808d
Esther Kibuu <kibuue@gmail.com>
2025-11-16 16:05:20 +0300

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.views.generic.detail import DetailView  # ← Only this line changed
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from .models import Library
 from .models import Book, UserProfile
 
@@ -71,3 +71,37 @@ def librarian_view(request):
 @user_passes_test(is_member, login_url='/relationship/login/')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+# Book management views with permissions
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    from .forms import BookForm
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/add_book.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    from .forms import BookForm
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('list_books')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/edit_book.html', {'form': form, 'book': book})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
