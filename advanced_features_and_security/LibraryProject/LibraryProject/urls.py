d482c0bb1807b70bb07c4fc8fbdd0f996f5226eb
Esther Kibuu <kibuue@gmail.com>
2025-11-16 21:03:36 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Only keep this one (with comma)
+    path('admin/', admin.site.urls),
+    path('bookshelf/', include('bookshelf.urls')),  # Add this line
     path('relationship/', include('relationship_app.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.contrib import messages
+from .models import Book
 
-# Create your views here.
+# List all books - accessible to users with can_view permission
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    """
+    Display a list of all books.
+    Requires: can_view permission
+    """
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# Create a new book - accessible to users with can_create permission
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    """
+    Create a new book.
+    Requires: can_create permission
+    """
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        publication_year = request.POST.get('publication_year')
+        
+        Book.objects.create(
+            title=title,
+            author=author,
+            publication_year=publication_year
+        )
+        messages.success(request, 'Book created successfully!')
+        return redirect('book_list')
+    
+    return render(request, 'bookshelf/book_form.html', {'action': 'Create'})
+
+# Edit an existing book - accessible to users with can_edit permission
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    """
+    Edit an existing book.
+    Requires: can_edit permission
+    """
+    book = get_object_or_404(Book, pk=pk)
+    
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.publication_year = request.POST.get('publication_year')
+        book.save()
+        
+        messages.success(request, 'Book updated successfully!')
+        return redirect('book_list')
+    
+    return render(request, 'bookshelf/book_form.html', {
+        'book': book,
+        'action': 'Edit'
+    })
+
+# Delete a book - accessible to users with can_delete permission
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    """
+    Delete a book.
+    Requires: can_delete permission
+    """
+    book = get_object_or_404(Book, pk=pk)
+    
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, 'Book deleted successfully!')
+        return redirect('book_list')
+    
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
